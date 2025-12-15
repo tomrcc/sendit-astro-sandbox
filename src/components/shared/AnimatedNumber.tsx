db9e92fd938fd1c337cca02abf561c4ad6bf3aee
Jan Claasen <jclaasen@netflixcontractors.com>
2025-12-16 10:28:31 +1300

    update editables and fix types

--- src/components/shared/AnimatedNumber.tsx
+++ src/components/shared/AnimatedNumber.tsx
@@ -59,7 +59,7 @@ const AnimatedNumber: React.FC<AnimatedNumberProps> = ({
       if (isInteger) {
         setDisplayNumber(Math.floor(currentValue));
       } else {
-        setDisplayNumber(currentValue.toFixed(2));
+        setDisplayNumber(parseFloat(currentValue.toFixed(2)));
       }
 
       if (progress < 1) {
